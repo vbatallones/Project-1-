1eb87e66a96eab144ad2b12867801006228e9a44
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-07-27 19:57:08 -0700

    [Readme & JS] Add more pictures and levels to my JS file

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 ## The Memory Game
 
+## How the game works
 
+* When the game starts, all tiles are turned face down.
+* The player then flips over two cards, selecting them by clicking on them.
+* If the two tiles have the same image, they remain face up. If not, they should be flipped face down again after a short delay.
 
 ## HTML
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Memory Game</title>
+</head>
+<body>
+    <h1 class="title">Trip Down To Memory Lane</h1>
+    <div class="countdown">Time:</div>
+    <div class="main-container" id="board">
+        <div class="card-container"></div>
+    </div>
+    <script src="./script.js"></script>
+</body>
+</html>
+```
+Add my boilerplate, with my CSS link and Javascript.
+My game will be timed which you can see in the `<div>` with a class of **countdown**. The board will be in the center of the screen as well the images. The images will be manipulated by the **DOM** and it will be all in my **JavaScript** file.
--- script.js
+++ script.js
@@ -1,38 +1,178 @@
-// const levelOneBoard = [
-//     {
-//         src: "https://img.icons8.com/color/48/000000/yoda.png",
-//         matched: false,
-//         id: 'Yoda'
-//     },
-//     {
-//         src: "https://img.icons8.com/color/48/000000/yoda.png",
-//         matched: false,
-//         id: 'Yoda'
-//     },
-//     {
-//         src: "https://img.icons8.com/color/48/000000/yoda.png",
-//         matched: false,
-//         id: 'Yoda'
-//     },
-//     {
-//         src: "https://img.icons8.com/color/48/000000/yoda.png",
-//         matched: false,
-//         id: 'Yoda'
-//     },
-//     {
-//         src: "https://img.icons8.com/color/48/000000/yoda.png",
-//         matched: false,
-//         id: 'Yoda'
-//     },
-//     {
-//         src: "https://img.icons8.com/color/48/000000/yoda.png",
-//         matched: false,
-//         id: 'Yoda'
-//     },
-//     {
-//         src: "https://img.icons8.com/color/48/000000/yoda.png",
-//         matched: false,
-//         id: 'Yoda'
-//     },
+const boardDeck = [
+    {
+        src: "https://img.icons8.com/color/48/000000/yoda.png",
+        matched: false,
+        id: 'Yoda'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/chewbacca.png",
+        matched: false,
+        id: 'Chewbacca'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/darth-vader.png",
+        matched: false,
+        id: 'Darth-vader'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/r2-d2.png",
+        matched: false,
+        id: 'R2-D2'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/animation.png",
+        matched: false,
+        id: 'Mickey'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/sonic-the-hedgehog-1.png",
+        matched: false,
+        id: 'Sonic'
+    },
+    {
+        src: "https://img.icons8.com/doodle/48/000000/super-mario.png",
+        matched: false,
+        id: 'Mario'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/pinball.png",
+        matched: false,
+        id: 'Pinball'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/bullbasaur.png",
+        matched: false,
+        id: 'Bulba'
+    },
+    {
+        src: "https://img.icons8.com/officel/40/000000/walter-white.png",
+        matched: false,
+        id: 'MrWhite'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/jigglypuff.png",
+        matched: false,
+        id: 'JigglyPuff'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/empire.png",
+        matched: false,
+        id: 'Empire'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/luke-skywalker.png",
+        matched: false,
+        id: 'Luke'
+    },
+    {
+        src: "https://img.icons8.com/office/40/000000/rebel.png",
+        matched: false,
+        id: 'Rebel'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/api.png",
+        matched: false,
+        id: 'API'
+    },
+    {
+        src: "https://img.icons8.com/officel/40/000000/popeye.png",
+        matched: false,
+        id: 'Popeye'
+    },
+    {
+        src: "https://img.icons8.com/officel/40/000000/olive-oyl.png",
+        matched: false,
+        id: 'Olive'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/finn.png",
+        matched: false,
+        id: 'Fin'
+    },
+    {
+        src: "https://img.icons8.com/officel/40/000000/jake.png",
+        matched: false,
+        id: 'Jake'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/pirates-of-the-caribbean.png",
+        matched: false,
+        id: 'Pirates'
+    },
+    {
+        src: "https://img.icons8.com/color/48/000000/snitch.png",
+        matched: false,
+        id: 'HarryP'
+    }
+]
+
+const board = document.querySelector('#board')
+const cardContainer = document.querySelector('.card-container')
+const body = document.querySelector('body')
+let checkBoard = [];
+
+// level one board
+const levelOneBoard = () => {
+    // this will give me 5 images out of the board
+    return boardDeck.splice(0,5);
+
+}
+// level two board 
+const levelTwoBoard = () => {
+    // this will give me 7 images out of the board
+    return boardDeck.splice(0,7);
+}
+//level three board
+const levelThreeBoard = () => {
+    // this will give me 10 images out of the board
+    return boardDeck.splice(0,10);
+}
+// level four board
+const levelFourBoard = () => {
+    // this will give me 15 images out of the board
+    return boardDeck.splice(0,15);
+}
+// level five board
+const levelFiveBoard = () => {
+    // this will give me 20 images out of the board
+    return boardDeck.splice(0);
+}
+// loop through images
+const gameLoop = levelboards => {
     
-// ]
+    for (let card of levelboards) {
+        // create an img element that will pass in the source and id of the object
+        const newCard = document.createElement('img')
+        newCard.src = card.src
+        newCard.id = card.id
+        card += newCard
+        card += newCard
+        //append the image to my board
+        cardContainer.appendChild(newCard)
+        checkBoard.push(cardContainer)
+        // duplicating my images into two, so I have 2 of each images.
+        let clonedItem = newCard.cloneNode(true)
+        cardContainer.appendChild(clonedItem)
+        
+        clonedItem.addEventListener('click', (e) => {
+            let idImg = e.target.id
+           if (idImg === idImg) {
+               cardContainer.style.display = 'block'
+           } else {
+
+           }
+          
+        })
+        newCard.addEventListener('click', (e) => {
+           let idImg = e.target.id
+           
+       })
+    }
+};
+gameLoop(levelTwoBoard())
+
+//levelOneBoard(boardDeck)
+
+
+
+
